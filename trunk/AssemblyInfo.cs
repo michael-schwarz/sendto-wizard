fc7be00a1d969640bb8dda9457cad8507c975ec2
mikebeblack <mikebeblack@4cfd5eea-a62c-4049-a3a8-83de7edef469>
2012-03-29 14:51:29 +0000

    version number changed

--- trunk/AssemblyInfo.cs
+++ trunk/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sendto Wizard 1.2")]
+[assembly: AssemblyTitle("Sendto Wizard 1.3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Michael Schwarz for programs.xe.cx")]
-[assembly: AssemblyProduct("Sendto Wizard 1.2")]
+[assembly: AssemblyProduct("Sendto Wizard 1.3")]
 [assembly: AssemblyCopyright("Michael Schwarz under GNU GPL v3 or later")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyVersion("1.3")]
